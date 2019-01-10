3fa3318bbfb85fdcd2716be850ec2e0aba20aa9a
tianwangchong <q408596288@vip.qq.com>
2019-01-10 16:13:46 +0800

    解决laravel5.4开始使用4个字节存字符

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // laravel 5.4 改变了默认的数据库字符集，现在 utf8mb4 包括存储 emojis 支持。如果你运行MySQL v5.7.7或者更高版本，则不需要做任何事情。相当于之前3个字节来存，现在用4个字节来存。
+        Schema::defaultStringLength(191);
     }
 
     /**

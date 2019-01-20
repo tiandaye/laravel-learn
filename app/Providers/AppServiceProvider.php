c8a667c53623e24d11d20ae4a7646a23136f771b
tianwangchong <q408596288@vip.qq.com>
2019-01-20 16:46:00 +0800

    引入horizon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,8 @@ use Log;
 use Monolog\Handler\RedisHandler;
 use Illuminate\Support\Facades\Redis;
 use Predis;
+use Laravel\Horizon\Horizon;
+use Admin;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Horizon::auth(function ($request) {
+            return true;
+        });
+
         // laravel 5.4 改变了默认的数据库字符集，现在 utf8mb4 包括存储 emojis 支持。如果你运行MySQL v5.7.7或者更高版本，则不需要做任何事情。相当于之前3个字节来存，现在用4个字节来存。
         Schema::defaultStringLength(191);
 
